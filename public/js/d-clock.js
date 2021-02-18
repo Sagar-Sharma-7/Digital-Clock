04550af4dafbc00c319733d47ed165e9f8d72a77
ike-fite <49214993+ike-fite@users.noreply.github.com>
2021-02-17 21:52:24 -0500

    Update d-clock.js
    Changed variables from global to local to abide by best practice. Changed DOM updates from vanilla javascript to Jquery. Generally cleaned up code.

--- public/js/d-clock.js
+++ public/js/d-clock.js
@@ -1,19 +1,13 @@
 Time();
 function Time() {
-    var date = new Date();
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
-    hour = updateTime(hour);
-    min = updateTime(min);
-    sec = updateTime(sec);
-    var $$time = setTimeout(Time, 1000);
-    document.getElementById("container-1").innerHTML = hour + " : " + min + " : " + sec;
-
-
-    
-    var day = date.getDate();
-    var month = ["January",
+    let date = new Date();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let $$time = setTimeout(Time, 1000);
+    $("#container-1").val(`${hour} : ${min} : ${sec}`)
+    let day = date.getDate();
+    let month = ["January",
                   "February",
                   "March",
                   "April",
@@ -25,21 +19,18 @@ function Time() {
                   "October",
                   "November",
                   "December"];
-    var year = date.getFullYear();
+    let year = date.getFullYear();
     day = updateTime(day);
     month = updateTime(month);
     year = updateTime(year);
     
-    document.getElementById("container-2").innerHTML = day + " - " + month[date.getMonth()] + " - " +  year ;
+   $("#container-2").val(`${day} - ${month[date.getMonth()]} - ${year}`);
 }
 
 
 function updateTime(t) {
-    if (t < 10) {
-        return "0" + t;
-    } else {
-        return t;
-    }
+    if (t < 10) return "0" + t;
+    else return t;
 }
 
 
